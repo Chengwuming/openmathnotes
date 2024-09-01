279bfd5317d408060d0acb69b063d2cafb44bc2a
Chengwuming <110581583+Chengwuming@users.noreply.github.com>
2024-09-01 10:44:53 +0800

    snake

--- Snake/input.js
+++ Snake/input.js
@@ -2,18 +2,17 @@ let inputDirection = { x: 0, y: 1 };
 let lastInputDirection = { x: 0, y: 1 };
 
 window.addEventListener('keydown', (event) => {
-    event.preventDefault();
-    
-    if (event.key === "w" && lastInputDirection.y !== 1) {
+    if (event.key === "ArrowUp" && lastInputDirection.y !== 1) {
         inputDirection = { x: 0, y: -1 };
-    } else if (event.key === "s" && lastInputDirection.y !== -1) {
+    } else if (event.key === "ArrowDown" && lastInputDirection.y !== -1) {
         inputDirection = { x: 0, y: 1 };
-    } else if (event.key === "d" && lastInputDirection.x !== -1) {
+    } else if (event.key === "ArrowRight" && lastInputDirection.x !== -1) {
         inputDirection = { x: 1, y: 0 };
-    } else if (event.key === "a" && lastInputDirection.x !== 1) {
+    } else if (event.key === "ArrowLeft" && lastInputDirection.x !== 1) {
         inputDirection = { x: -1, y: 0 };
     }
-})
+});
+
 
 const getInputDirection = () => {
     lastInputDirection = inputDirection;
